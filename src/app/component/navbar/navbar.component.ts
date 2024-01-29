10aa41aafa373d5ff1500ec2a05b9c5912e978b2
Wafaa <Wafaabahgat>
2024-01-30 01:58:59 +0300

    vv

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BgDirective } from '../../CustomDirectives/bg.directive';
+import { flush } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, BgDirective, RouterLinkActive],
+  imports: [RouterLink, BgDirective, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isItemsShowed: boolean = false;
+  showitems(): void {
+    console.log('open navigation menu');
+    this.isItemsShowed = !this.isItemsShowed;
+  }
+  
+  closeNav(): void {
+    console.log('Closing navigation menu');
+    this.isItemsShowed = false;
+  }
+}
